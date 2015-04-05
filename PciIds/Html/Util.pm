3304a1483ad76f06e39702fe7ecc9c017b2232eb
Martin Mares <mj@ucw.cz>
2015-04-05 23:21:44 +0200

    Get rid of SF and AnyBrowser logos
    They are obsolete and the SF logo was served through http even
    at our https pages.

--- PciIds/Html/Util.pm
+++ PciIds/Html/Util.pm
@@ -61,8 +61,6 @@ sub genHtmlFooter( $$$ ) {
 	}
 	print '
 <div class="footer"><hr>
-<div class="campaign"><a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=35833" width="88" height="31" alt="SourceForge"></a></div>
-<div class="campaign"><a href="http://www.anybrowser.org/campaign/"><img src="/static/anybrowser.png" width=88 height=31 alt="Best viewed with any browser."></a></div>
 <p class="maintain">
 Maintained by <a href="http://mj.ucw.cz/">Martin Mares</a> and <a href="http://vorner.cz/">Michal
 Vaner</a> with great help by volunteers from the <a href="http://www.sourceforge.net/projects/pciids/">pciids project</a>
