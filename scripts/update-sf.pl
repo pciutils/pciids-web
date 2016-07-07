9033791892966ad6c5abce91f3744380e76fbf3e
The PCI ID Mail Robot <pci-admins@ucw.cz>
2016-07-07 19:52:35 +0200

    update-sf

--- scripts/update-sf.pl
+++ scripts/update-sf.pl
@@ -8,8 +8,6 @@ cd htdocs
 O=~/ids/docs
 cp -aL $O/static .
 cp -aL $O/{v2.2,pci.ids*} .
-sed <$O/index.html >index.html -e '
-	s@href="/\(read\|mods\)/@href="http://pci-ids.ucw.cz/\1/@g
-'
+cp ../index.html .
 
-rsync -avz --delete ~/sf.net/htdocs mares,pciids@web.sourceforge.net:
+rsync -az --delete ~/sf.net/htdocs mares,pciids@web.sourceforge.net:
